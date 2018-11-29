e7c99ac13bd953cc2f273ca585f2e795c4728f24
Enisvaldo <danilo12coelho@gmail.com>
2018-11-29 15:17:45 -0300

    Primeiro projeto

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Contato
+class ContatoAdmin(admin.ModelAdmin):
+    list_display =  ('nome', 'email', 'telefone')
+admin.site.register(Contato, ContatoAdmin)
